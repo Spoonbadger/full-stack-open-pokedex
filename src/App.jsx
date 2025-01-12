e0d6af1811ed1b9510e855657452c08a37f7f7d1
Spoonbadger <craig.adam.morley@gmail.com>
2025-01-11 20:06:42 -0800

    fixed test adding react import

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
+import React from 'react'
 
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
   url,
